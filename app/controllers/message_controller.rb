193908987cf5818a71966dc0ec8adb34d9b6dfb2
masahiro kamono <masahiro.kamono@gmail.com>
2016-04-12 22:49:07 +0000

    create messages_controller

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,4 +1,19 @@
 class MessageController < ApplicationController
   def index
+    @message = Message.new
+    #全てのメッセージを取得
+    @messages = Message.all
   end
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました。'
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  ##ここまで
 end
