25f68ea7208c51156ad8795611991a78b225af77
masahiro kamono <masahiro.kamono@gmail.com>
2016-04-13 13:35:59 +0000

    create message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,10 @@
-class Message < ActiveRecord::Base
+class CreateMessages < ActiveRecord::Migration
+    def change
+        create_table :messages do |t|
+            t.string :name
+            t.string :body
+            
+            t.timestamps null: false
+        end
+    end
 end
