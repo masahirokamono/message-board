c3f52f02f199e73cd015070df44e7550feca71d2
masahiro kamono <masahiro.kamono@gmail.com>
2016-04-25 22:21:10 +0000

    sixth

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,8 @@ class Message < ActiveRecord::Base
   class Message < ActiveRecord::Base
   # 名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence: true
-  # 年齢は２桁
-  validates :age , numericality: { only_integer: true, greater_than_to: 1} , presence: true 
+  # 年齢は１桁
+  validates :age , numericality: { only_integer: true, greater_than_to: 0} , presence: true 
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
   end
